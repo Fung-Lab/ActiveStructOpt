f6a8d97f4879d2fec40e366622d50dbf53bf23c3
itsgt <islagle3@gatech.edu>
2023-12-15 13:51:11 -0500

    Add perturbations of lattice to data generation

--- activestructopt/dataset/dataset.py
+++ activestructopt/dataset/dataset.py
@@ -4,6 +4,8 @@ import numpy as np
 
 def make_data_splits(initial_structure, optfunc, args, config, 
                       perturbrmin = 0.1, perturbrmax = 1.0, 
+                      perturblmin = -0.2, perturblmax = 0.2,
+                      perturbθmin = -5, perturbθmax = 5,
                       N = 100, split = 0.85, k = 5, device = 'cuda'):
   structures = [initial_structure.copy() for _ in range(N)]
   for i in range(1, N):
@@ -11,10 +13,25 @@ def make_data_splits(initial_structure, optfunc, args, config,
     while rejected:
       new_structure = initial_structure.copy()
       new_structure.perturb(np.random.uniform(perturbrmin, perturbrmax))
+      new_structure.lattice = new_structure.lattice.from_parameters(
+        max(0.0, new_structure.lattice.a + np.random.uniform(
+          perturblmin, perturblmax)),
+        max(0.0, new_structure.lattice.b + np.random.uniform(
+          perturblmin, perturblmax)),
+        max(0.0, new_structure.lattice.c + np.random.uniform(
+          perturblmin, perturblmax)), 
+        min(180.0, max(0.0, new_structure.lattice.alpha + np.random.uniform(
+          perturbθmin, perturbθmax))), 
+        min(180.0, max(0.0, new_structure.lattice.beta + np.random.uniform(
+          perturbθmin, perturbθmax))), 
+        min(180.0, max(0.0, new_structure.lattice.gamma + np.random.uniform(
+          perturbθmin, perturbθmax)))
+      )
       rejected = lj_reject(new_structure)
     structures[i] = new_structure.copy()
   ys = [optfunc(structures[i], **(args)) for i in range(N)]
-  data = [prepare_data(structures[i], config, y = ys[i]).to(device) for i in range(N)]
+  data = [prepare_data(structures[i], config, y = ys[i]).to(
+    device) for i in range(N)]
       
   structure_indices = np.random.permutation(np.arange(1, N))
   trainval_indices = structure_indices[:int(np.floor(split * N) - 1)]
@@ -24,10 +41,10 @@ def make_data_splits(initial_structure, optfunc, args, config,
   test_data = [data[i] for i in test_indices]
   test_targets = [ys[i] for i in test_indices]
   train_indices = [np.concatenate(
-      [kfolds[j] for j in range(k) if j != i]) for i in range(k)]
+    [kfolds[j] for j in range(k) if j != i]) for i in range(k)]
   
   datasets = [([data[j] for j in train_indices[i]], 
-      [data[j] for j in kfolds[i]]) for i in range(k)]
+    [data[j] for j in kfolds[i]]) for i in range(k)]
   
   return structures, ys, datasets, kfolds, test_indices, test_data, test_targets
 
