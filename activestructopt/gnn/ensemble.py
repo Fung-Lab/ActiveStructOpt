bf38cfd6abc6c4d25d281c15889a024c4a194fd9
itsgt <islagle3@gatech.edu>
2024-01-21 11:09:38 -0500

    MDL ensemble always has one

--- activestructopt/gnn/ensemble.py
+++ activestructopt/gnn/ensemble.py
@@ -89,12 +89,9 @@ class Ensemble:
 
             # Compute forward, loss, backward    
             with autocast(enabled=self.ensemble[j].trainer.use_amp):
-              out_list = self.ensemble[j].trainer._forward(batch)
-              print(out_list)
-              raise Exception('stop', 'here please')                                             
+              out_list = self.ensemble[j].trainer._forward(batch)                                       
               loss = self.ensemble[j].trainer._compute_loss(out_list, batch) 
-            for i in range(len(self.ensemble[j].trainer.model)):
-              self.ensemble[j].trainer._backward(loss[i], i)
+            self.ensemble[j].trainer._backward(loss[0], i)
 
             # Compute metrics
             _metrics[j] = self.ensemble[j].trainer._compute_metrics(out_list[0],
