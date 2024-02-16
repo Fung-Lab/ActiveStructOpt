1aa57a2b316e409143ffca009f6623900529f7da
Ian Slagle <islagle3@gatech.edu>
2024-02-16 11:01:17 -0500

    Optimize over cell also

--- activestructopt/optimization/basinhopping/basinhopping.py
+++ activestructopt/optimization/basinhopping/basinhopping.py
@@ -5,6 +5,7 @@ from activestructopt.optimization.shared.constraints import lj_rmins, lj_repulsi
 from matdeeplearn.preprocessor.helpers import (
     calculate_edges_master,
 )
+from pymatgen.core import Lattice
 
 def run_adam(ensemble, target, starting_structures, config, ljrmins,
                     niters = 100, λ = 1.0, lr = 0.01, device = 'cpu'):
@@ -12,12 +13,15 @@ def run_adam(ensemble, target, starting_structures, config, ljrmins,
   natoms = len(starting_structures[0])
   best_ucb = torch.tensor([float('inf')], device = device)
   best_x = torch.zeros(3 * natoms, device = device)
+  best_cell = torch.zeros((3, 3), device = device) 
   target = torch.tensor(target, device = device)
   data = [prepare_data(s, config, pos_grad = True).to(device) for s in starting_structures]
   for i in range(nstarts):
     data[i].pos = torch.tensor(starting_structures[i].lattice.get_cartesian_coords(
         starting_structures[i].frac_coords), device = device, dtype = torch.float)
-  optimizer = torch.optim.Adam([d.pos for d in data], lr=lr)
+    data[i].cell = torch.tensor(starting_structures[i].lattice.matrix, 
+                                device = device, dtype = torch.float).unsqueeze(0)
+  optimizer = torch.optim.Adam([d.pos for d in data], [d.cell for d in data], lr=lr)
   for i in range(niters):
     optimizer.zero_grad(set_to_none=True)
     for j in range(nstarts):
@@ -53,6 +57,7 @@ def run_adam(ensemble, target, starting_structures, config, ljrmins,
     if (torch.min(ucbs) < best_ucb).item():
       best_ucb = torch.min(ucbs).detach()
       best_x = data[torch.argmin(ucbs).item()].pos.detach().flatten()
+      best_cell = data[torch.argmin(ucbs).item()].cell[0].detach()
     predictions = predictions.detach()
     data[j].pos = data[j].pos.detach()
     ucb = ucb.detach()
@@ -60,8 +65,8 @@ def run_adam(ensemble, target, starting_structures, config, ljrmins,
     s = s.detach()
     del predictions, ucbs, ucb, yhat, s, edge_gen_out
     
-  to_return = best_x.detach().cpu().numpy() 
-  del best_ucb, best_x, target, data
+  to_return = best_x.detach().cpu().numpy(), best_cell.detach().cpu().numpy()
+  del best_ucb, best_x, best_cell, target, data
   return to_return
 
 def basinhop(ensemble, starting_structures, target, config,
@@ -70,10 +75,11 @@ def basinhop(ensemble, starting_structures, target, config,
   device = ensemble.device
   ljrmins = torch.tensor(lj_rmins, device = device)
 
-  new_x = run_adam(ensemble, target, starting_structures, config, ljrmins, 
+  new_x, new_cell = run_adam(ensemble, target, starting_structures, config, ljrmins, 
     niters = niters, λ = λ, lr = lr, device = device)
   
   new_structure = starting_structures[0].copy()
+  new_structure.lattice = Lattice(new_cell)
   for i in range(len(new_structure)):
     new_structure[i].coords = new_x[(3 * i):(3 * (i + 1))]
   return new_structure
