a84a4f81ea7fa466401667575b205c87e273e32a
Ian Slagle <islagle3@gatech.edu>
2024-11-01 14:30:45 -0400

    Implement basin hopping properly

--- activestructopt/optimizer/torch_bh.py
+++ activestructopt/optimizer/torch_bh.py
@@ -24,32 +24,46 @@ class TorchBH(BaseOptimizer):
     constraint_scale = 1.0, pos_lr = 0.001, cell_lr = 0.001,
     perturbrmin = 0.0, perturbrmax = 0.2, perturblσ = 0.1,
     **kwargs) -> IStructure:
-    
-    starting_structure = dataset.structures[0]
+
     device = model.device
-    natoms = len(starting_structure)
-    ljrmins = torch.tensor(lj_rmins, device = device)
+    
+    structure = dataset.structures[0].copy()
+    prev_structure = structure.copy()
+    target = torch.tensor(dataset.target, device = device)
+
     best_obj = torch.tensor([float('inf')], device = device)
     if optimize_atoms:
       best_x = torch.zeros(3 * natoms, device = device)
     if optimize_lattice:
       best_cell = torch.zeros((3, 3), device = device)
-    target = torch.tensor(dataset.target, device = device)
-    
-    data = prepare_data_pmg(starting_structure, dataset.config, 
-      pos_grad = optimize_atoms, cell_grad = optimize_lattice,
-      device = device, preprocess = False)
-    reprocess_data(data, dataset.config, device, edges = False)
-    
-    to_optimize = []
-    if optimize_atoms:
-      to_optimize += [{'params': data.pos, 'lr': pos_lr}]
-    if optimize_lattice:
-      to_optimize += [{'params': data.cell, 'lr': cell_lr}]
-    optimizer = getattr(torch.optim, optimizer)(to_optimize, 
-      **(optimizer_args))
 
-    for i in range(hops):
+    rmc_sampler = SingleAtomPerturbation(structure, perturbrmin = σr, 
+      perturbrmax = σr, perturblmax = 0, perturbθmax = 0, 
+      lattice_prob = 0)
+
+    #obj_vals = None
+    #if save_obj_values:
+    #  obj_vals = torch.zeros((iters_per_start, starts), device = 'cpu')
+
+    prev_obj = torch.tensor([float('inf')], device = device)
+
+    for _ in range(hops):
+      data = [prepare_data_pmg(structure, dataset.config, pos_grad = False, 
+        device = device, preprocess = True, cell_grad = False
+        )]
+
+      to_optimize = []
+      if optimize_atoms:
+        to_optimize += [{'params': data.pos, 'lr': pos_lr}]
+      if optimize_lattice:
+        to_optimize += [{'params': data.cell, 'lr': cell_lr}]
+      optimizer = getattr(torch.optim, optimizer)(to_optimize, 
+        **(optimizer_args))
+
+      best_local_obj = torch.tensor([float('inf')], device = device)
+      best_local_x = None
+      best_local_cell = None
+      
       for j in range(iters_per_hops):
         optimizer.zero_grad()
         if optimize_atoms:
@@ -63,69 +77,73 @@ class TorchBH(BaseOptimizer):
           N = 1)
         obj_total += constraint_scale * lj_repulsion(data, ljrmins)
         obj_total = torch.nan_to_num(obj_total, nan = torch.inf)
-        if (obj_total < best_obj).item():
-          best_obj = obj_total.detach()
+        if (obj_total < best_local_obj).item():
+          best_local_obj = obj_total.detach()
           if optimize_atoms:
-            best_x = data.pos.detach().flatten()
+            best_local_x = data.pos.detach().flatten()
           if optimize_lattice:
-            best_cell = data.cell[0].detach()
+            best_local_cell = data.cell[0].detach()
+          if (obj_total < best_obj).item():
+            best_obj = best_local_obj
+            if optimize_atoms:
+              best_x = best_local_x
+            if optimize_lattice:
+              best_cell = best_local_cell
         if j != iters_per_hops - 1:
           obj_total.backward()
           optimizer.step()
+
+      new_obj = best_local_obj
       
-      rejected = True
-      new_structure = starting_structure.copy()
-      if optimize_atoms:
-        new_x = data.pos.detach().flatten().cpu().numpy()
-      if optimize_lattice:
-        new_cell = data.cell[0].detach().cpu().numpy()
-
-      if optimize_lattice:
-        new_structure.lattice = Lattice(new_cell)
-      if optimize_atoms:
-        for i in range(len(new_structure)):
-          try:
-            new_structure[i].coords = new_x[(3 * i):(3 * (i + 1))]
-          except np.linalg.LinAlgError as e:
-            print(best_obj)
-            print(new_cell)
-            print(new_structure.lattice)
-            print(new_x)
-            raise e
-
-      while rejected:
-        try:
-          hop_structure = new_structure.copy()
-          hop_structure.perturb(np.random.uniform(perturbrmin, perturbrmax))
-          hop_structure.lattice = Lattice(hop_structure.lattice.matrix + 
-            perturblσ * np.random.normal(0, 1, (3, 3)))
-          rejected = lj_reject(hop_structure)
-        except:
-          rejected = True
-
-      data = prepare_data_pmg(hop_structure, dataset.config, 
-        pos_grad = optimize_atoms, cell_grad = optimize_lattice,
-        device = device, preprocess = False)
-      reprocess_data(data, dataset.config, device, edges = False)
+      Δobjs = new_obj - prev_obj
+      better = Δobjs <= 0
+      hastings = torch.log(torch.rand(1, device = device)) < Δobjs / (
+        -2 * σ ** 2)
+      accept = torch.logical_or(better, hastings)
+      if (accept).item():
+        if optimize_atoms:
+          new_x = best_local_x.cpu().numpy()
+        if optimize_lattice:
+          new_cell = best_local_cell.cpu().numpy()
+        
+        prev_structure = starting_structure.copy()
+        
+        if optimize_lattice:
+          prev_structure.lattice = Lattice(new_cell)
+        if optimize_atoms:
+          for i in range(len(prev_structure)):
+            try:
+              prev_structure[i].coords = new_x[(3 * i):(3 * (i + 1))]
+            except np.linalg.LinAlgError as e:
+              print(best_obj)
+              print(new_cell)
+              print(prev_structure.lattice)
+              print(new_x)
+              raise e
+        prev_structure = structure.copy()
+        prev_obj = obj_total
+    
+      rmc_sampler.initial_structure = prev_structure.copy()
+      structure = rmc_sampler.sample()
 
     if optimize_atoms:
       new_x = best_x.detach().cpu().numpy()
     if optimize_lattice:
       new_cell = best_cell.detach().cpu().numpy()
 
-    new_structure = starting_structure.copy()
+    best_structure = starting_structure.copy()
 
     if optimize_lattice:
-      new_structure.lattice = Lattice(new_cell)
+      best_structure.lattice = Lattice(new_cell)
     if optimize_atoms:
-      for i in range(len(new_structure)):
+      for i in range(len(best_structure)):
         try:
-          new_structure[i].coords = new_x[(3 * i):(3 * (i + 1))]
+          best_structure[i].coords = new_x[(3 * i):(3 * (i + 1))]
         except np.linalg.LinAlgError as e:
           print(best_obj)
           print(new_cell)
-          print(new_structure.lattice)
+          print(best_structure.lattice)
           print(new_x)
           raise e
-
-    return new_structure, None
+      
+    return best_structure, None
