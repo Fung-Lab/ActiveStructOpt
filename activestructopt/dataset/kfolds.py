47452c733d45a7e272a4e3422f16a65044a2782e
itsgt <islagle3@gatech.edu>
2024-11-28 18:29:27 -0500

    Use old kfold dataset

--- activestructopt/dataset/kfolds.py
+++ activestructopt/dataset/kfolds.py
@@ -12,9 +12,9 @@ import copy
 class KFoldsDataset(BaseDataset):
   def __init__(self, simulation: BaseSimulation, sampler: BaseSampler, 
     initial_structure: IStructure, target, config, N = 100, split = 0.85, 
-    k = 5, seed = 0, progress_dict = None, 
-    new_ys = None, wait_for_ys = True, **kwargs) -> None:
+    k = 5, device = 'cuda', seed = 0, progress_dict = None, **kwargs) -> None:
     np.random.seed(seed)
+    self.device = device
     self.config = config
     self.target = target
     self.initial_structure = initial_structure
@@ -22,7 +22,6 @@ class KFoldsDataset(BaseDataset):
     self.N = N
     self.k = k
     self.simfunc = simulation
-    self.wait_for_ys = wait_for_ys
 
     if progress_dict is None:
       self.structures = [initial_structure.copy(
@@ -31,54 +30,65 @@ class KFoldsDataset(BaseDataset):
       y_promises = [copy.deepcopy(simulation) for _ in self.structures]
       for i, s in enumerate(self.structures):
         y_promises[i].get(s)
+      self.ys = [yp.resolve() for yp in y_promises]
+      data = [prepare_data_pmg(self.structures[i], config, y = self.ys[i]).to(
+        self.device) for i in range(N)]
           
       structure_indices = np.random.permutation(np.arange(1, N))
       trainval_indices = structure_indices[:int(np.round(split * N) - 1)]
       trainval_indices = np.append(trainval_indices, [0])
       self.kfolds = np.array_split(trainval_indices, k)
+      for i in range(self.k):
+        self.kfolds[i] = self.kfolds[i].tolist()
       self.test_indices = structure_indices[int(np.round(split * N) - 1):]
+      self.test_data = [data[i] for i in self.test_indices]
+      self.test_targets = [self.ys[i] for i in self.test_indices]
+      train_indices = [np.concatenate(
+        [self.kfolds[j] for j in range(k) if j != i]) for i in range(k)]
+      
+      self.datasets = [([data[j] for j in train_indices[i]], 
+        [data[j] for j in self.kfolds[i]]) for i in range(k)]
 
-      if self.wait_for_ys:
-        self.ys = [yp.resolve() for yp in y_promises]
-        self.test_targets = [self.ys[i] for i in self.test_indices]
-        self.mismatches = [simulation.get_mismatch(y, target) for y in self.ys]
-      else:
-        self.ys = []
-        self.mismatches = []
+      self.mismatches = [simulation.get_mismatch(y, target) for y in self.ys]
     else:
       self.start_N = progress_dict['start_N']
       self.N = progress_dict['N']
       self.structures = [Structure.from_dict(
         s) for s in progress_dict['structures']]
       self.ys = [np.array(y) for y in progress_dict['ys']]
-      if not (new_ys is None):
-        self.ys.extend(new_ys)
       self.kfolds = progress_dict['kfolds']
       self.test_indices = np.array(progress_dict['test_indices'])
       self.mismatches = progress_dict['mismatches']
+      data = [prepare_data_pmg(self.structures[i], config, y = self.ys[i]).to(
+        self.device) for i in range(len(self.structures))]
+      trainval_indices = np.setxor1d(np.arange(len(self.structures)), self.test_indices)
+      self.datasets = [([data[j] for j in np.setxor1d(trainval_indices, self.kfolds[i])], 
+        [data[j] for j in self.kfolds[i]]) for i in range(k)]
+      self.test_data = [data[i] for i in self.test_indices]
+      self.test_targets = [self.ys[i] for i in self.test_indices]
+
 
   def update(self, new_structure: IStructure):
     self.structures.append(new_structure)
     y_promise = self.simfunc
     y_promise.get(new_structure)
-    self.N += 1
-
-    if not self.wait_for_ys:
-      y = y_promise.resolve()
-      new_mismatch = self.simfunc.get_mismatch(y, self.target)
-      y_promise.garbage_collect(new_mismatch <= min(self.mismatches))
-      self.update_new_ys(self, y)
-
-  def update_new_ys(self, new_y):
+    y = y_promise.resolve()
+    new_mismatch = self.simfunc.get_mismatch(y, self.target)
+    y_promise.garbage_collect(new_mismatch <= min(self.mismatches))
+    new_data = prepare_data_pmg(new_structure, self.config, y = y).to(self.device)
     fold = len(self.datasets) - 1
     for i in range(len(self.datasets) - 1):
       if len(self.datasets[i][1]) < len(self.datasets[i + 1][1]):
         fold = i
         break
-    self.kfolds[fold].append(fold)
-    new_mismatch = self.simfunc.get_mismatch(new_y, self.target)
-    self.ys.append(new_y)
+    self.kfolds[fold].append(len(self.structures))
+    self.datasets[fold][1].append(new_data)
+    for i in range(len(self.datasets)):
+      if fold != i:
+        self.datasets[i][0].append(new_data)
+    self.ys.append(y)
     self.mismatches.append(new_mismatch)
+    self.N += 1
 
   def toJSONDict(self):
     return {
