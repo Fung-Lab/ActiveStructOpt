5f9229b2337bfcc7dfe11b06baa81b24c97736af
itsgt <islagle3@gatech.edu>
2025-02-12 15:54:22 -0500

    Fix get_mismatch in rdf_d

--- activestructopt/simulation/rdf_d.py
+++ activestructopt/simulation/rdf_d.py
@@ -53,4 +53,4 @@ class RDFD(BaseSimulation):
     return
 
   def get_mismatch(self, to_compare, target):
-    return torch.mean((to_compare - target) ** 2).item()
+    return torch.mean((np.mean(to_compare, axis = 0) - target) ** 2).item()
