2b8004331076ac35663df83993e5a78707cea800
itsgt <islagle3@gatech.edu>
2025-08-24 12:31:24 -0400

    Make RMCList work with progress_dict

--- activestructopt/dataset/rmc_list.py
+++ activestructopt/dataset/rmc_list.py
@@ -2,7 +2,7 @@ from activestructopt.simulation.base import BaseSimulation, ASOSimulationExcepti
 from activestructopt.dataset.base import BaseDataset
 from activestructopt.sampler.base import BaseSampler
 from activestructopt.common.registry import registry
-from pymatgen.core.structure import IStructure
+from pymatgen.core.structure import IStructure, Structure
 import numpy as np
 import copy
 import time
@@ -11,7 +11,8 @@ import time
 class RMCList(BaseDataset):
   def __init__(self, simulations: list[BaseSimulation], sampler: BaseSampler, 
     initial_structure: IStructure, targets, config, seed = 0, σ = 0.0025, 
-    max_sim_calls = 5, call_sequential = False, weights = None, **kwargs) -> None:
+    max_sim_calls = 5, call_sequential = False, weights = None, progress_dict = None,
+    **kwargs) -> None:
     np.random.seed(seed)
     self.config = config
     self.targets = targets
@@ -21,62 +22,80 @@ class RMCList(BaseDataset):
     self.simfuncs = simulations
     self.weights = weights
 
-    self.structures = [initial_structure.copy()]
-    
-    self.ys = [[None for _ in range(self.N)] for _ in range(len(
-      self.simfuncs))]
-    
-    sim_calls = 0
+    if progress_dict is None:
+      self.structures = [initial_structure.copy()]
+      
+      self.ys = [[None for _ in range(self.N)] for _ in range(len(
+        self.simfuncs))]
+      
+      sim_calls = 0
 
-    y_promises = [[copy.deepcopy(self.simfuncs[j]
-      ) for _ in self.structures] for j in range(len(self.simfuncs))]
-    if not call_sequential:
-      for i, s in enumerate(self.structures):
-        time.sleep(5)
-        for j in range(len(self.simfuncs)):
-          y_promises[j][i].get(s, group = True, separator = ' ')
-    self.mismatches = [[np.NaN for _ in range(len(self.structures)
-      )] for _ in range(len(self.simfuncs))]
+      y_promises = [[copy.deepcopy(self.simfuncs[j]
+        ) for _ in self.structures] for j in range(len(self.simfuncs))]
+      if not call_sequential:
+        for i, s in enumerate(self.structures):
+          time.sleep(5)
+          for j in range(len(self.simfuncs)):
+            y_promises[j][i].get(s, group = True, separator = ' ')
+      self.mismatches = [[np.NaN for _ in range(len(self.structures)
+        )] for _ in range(len(self.simfuncs))]
 
-    while self.sims_incomplete():
-      sim_calls += 1
-      sim_updated = False
-      for i in range(self.N):
-        if self.sims_incomplete(s = i):
-          sim_updated = True
-          try:
-            for j in range(len(self.simfuncs)):
-              if call_sequential:
-                y_promises[j][i].get(self.structures[i])
-              self.ys[j][i] = y_promises[j][i].resolve()
-              self.mismatches[j][i] = y_promises[j][i].get_mismatch(
-                self.ys[j][i], targets[j])
-              if self.mismatches[j][i] <= np.nanmin(self.mismatches[j]):
-                for k in range(self.N):
-                  if type(self.ys[j][k]) != type(None) and i != k:
-                    y_promises[j][k].garbage_collect(False)
-              else:
-                y_promises[j][i].garbage_collect(False)
-          except ASOSimulationException:
-            if sim_calls <= max_sim_calls:
-              # resample and try again
-              print(f'retrying structure {i}')
-              self.structures[i] = sampler.sample()
+      while self.sims_incomplete():
+        sim_calls += 1
+        sim_updated = False
+        for i in range(self.N):
+          if self.sims_incomplete(s = i):
+            sim_updated = True
+            try:
               for j in range(len(self.simfuncs)):
-                y_promises[j][i].garbage_collect(False)
-                y_promises[j][i] = copy.deepcopy(self.simfuncs[j])
-                if not call_sequential:
-                  time.sleep(5)
-                  y_promises[j][i].get(self.structures[i], group = True, 
-                    separator = ' ')
-            else:
-              raise Exception(f'Max sim calls reached for structure {i}')
-      assert sim_updated
+                if call_sequential:
+                  y_promises[j][i].get(self.structures[i])
+                self.ys[j][i] = y_promises[j][i].resolve()
+                self.mismatches[j][i] = y_promises[j][i].get_mismatch(
+                  self.ys[j][i], targets[j])
+                if self.mismatches[j][i] <= np.nanmin(self.mismatches[j]):
+                  for k in range(self.N):
+                    if type(self.ys[j][k]) != type(None) and i != k:
+                      y_promises[j][k].garbage_collect(False)
+                else:
+                  y_promises[j][i].garbage_collect(False)
+            except ASOSimulationException:
+              if sim_calls <= max_sim_calls:
+                # resample and try again
+                print(f'retrying structure {i}')
+                self.structures[i] = sampler.sample()
+                for j in range(len(self.simfuncs)):
+                  y_promises[j][i].garbage_collect(False)
+                  y_promises[j][i] = copy.deepcopy(self.simfuncs[j])
+                  if not call_sequential:
+                    time.sleep(5)
+                    y_promises[j][i].get(self.structures[i], group = True, 
+                      separator = ' ')
+              else:
+                raise Exception(f'Max sim calls reached for structure {i}')
+        assert sim_updated
 
-    self.curr_structure = self.structures[0]
-    self.curr_mismatch = self.mismatches[0]
-    self.accepted = [True]
-    self.σ = σ
+      self.curr_structure = self.structures[0]
+      new_mismatch = 0
+      for j in range(len(self.simfuncs)):  
+        new_mismatch += self.mismatches[0][j] if self.weights is None else (
+          self.mismatches[0][j] * self.weights[j])
+      self.curr_mismatch = new_mismatch
+      self.accepted = [True]
+      self.σ = σ
+    else:
+      pd_ys = progress_dict['ys']
+      for i in range(len(pd_ys)):
+        for j in range(len(pd_ys[i])):
+          pd_ys[i][j] = np.array(pd_ys[i][j])
+      self.ys = pd_ys
+      self.mismatches = progress_dict['mismatches']
+      self.start_N = progress_dict['start_N']
+      self.N = progress_dict['N']
+      self.structures = []
+      self.curr_structure = Structure.from_dict(progress_dict['curr_structure'])
+      self.curr_mismatch = progress_dict['curr_mismatch']
+      self.accepted = progress_dict['accepted']
 
   def update(self, new_structure: IStructure):
     new_ys = [None for _ in range(len(self.simfuncs))]
@@ -126,5 +145,8 @@ class RMCList(BaseDataset):
       'structures': [s.as_dict() for s in self.structures] if (
         save_structures) else self.structures[np.argmin(self.mismatches)].as_dict(),
       'ys': ys_to_save,
-      'mismatches': self.mismatches
+      'mismatches': self.mismatches,
+      'curr_structure': self.curr_structure,
+      'curr_mismatch': self.curr_mismatch,
+      'accepted': self.accepted,
     }
