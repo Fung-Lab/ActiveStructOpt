9e0e50d8e9056110ac4b0725ffb4d21340411c27
Ian Slagle <islagle3@gatech.edu>
2025-11-21 15:48:32 -0500

    Add batch_pos_cell

--- activestructopt/model/groundtruth.py
+++ activestructopt/model/groundtruth.py
@@ -1,6 +1,7 @@
 from activestructopt.model.base import BaseModel
 from activestructopt.common.registry import registry
 from activestructopt.dataset.base import BaseDataset
+from pymatgen.core import Lattice
 import torch
 
 @registry.register_model("GroundTruth")
@@ -11,6 +12,16 @@ class GroundTruth(BaseModel):
 
   def train(self, dataset: BaseDataset, **kwargs):
     return None, None, torch.empty(0)
+
+  def batch_pos_cell(pos, cells, init_struct):
+    structs = []
+    for i in range(len(pos)):
+      new_struct = init_struct.copy()
+      new_struct.lattice = Lattice(cell.detach().cpu().numpy())
+      for j in range(len(new_struct)):
+        new_struct.sites[i].coords = pos[i][j].detach().cpu().numpy()
+      structs.append(new_struct)
+    return structs
 
   def predict(self, data, **kwargs):
     gt = self.simfunc.get_and_resolve_prepared(data)
