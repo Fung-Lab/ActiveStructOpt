6bb1f8a78d42cc6bf6a61fc0bac854ade4232e59
Ian Slagle <islagle3@gatech.edu>
2025-11-21 15:52:22 -0500

    Remove batch_pos_cell

--- activestructopt/model/groundtruth.py
+++ activestructopt/model/groundtruth.py
@@ -13,16 +13,6 @@ class GroundTruth(BaseModel):
   def train(self, dataset: BaseDataset, **kwargs):
     return None, None, torch.empty(0)
 
-  def batch_pos_cell(pos, cells, init_struct):
-    structs = []
-    for i in range(len(pos)):
-      new_struct = init_struct.copy()
-      new_struct.lattice = Lattice(cell.detach().cpu().numpy())
-      for j in range(len(new_struct)):
-        new_struct.sites[i].coords = pos[i][j].detach().cpu().numpy()
-      structs.append(new_struct)
-    return structs
-
   def predict(self, data, **kwargs):
     gt = self.simfunc.get_and_resolve_prepared(data)
     unc = torch.zeros(gt.size(), device = gt.device)
