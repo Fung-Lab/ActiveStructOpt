9720dac64f6d950a31575e6141a94673f7709da8
Ian Slagle <islagle3@gatech.edu>
2026-07-15 22:01:47 -0400

    Add GroundTruth predictions for where data is not prepared

--- activestructopt/model/groundtruth.py
+++ activestructopt/model/groundtruth.py
@@ -3,6 +3,7 @@ from activestructopt.common.registry import registry
 from activestructopt.dataset.base import BaseDataset
 from pymatgen.core import Lattice
 import torch
+import copy
 
 @registry.register_model("GroundTruth")
 class GroundTruth(BaseModel):
@@ -13,8 +14,17 @@ class GroundTruth(BaseModel):
   def train(self, dataset: BaseDataset, **kwargs):
     return None, None, torch.empty(0)
 
-  def predict(self, data, **kwargs):
-    gt = self.simfunc.get_and_resolve_prepared(data)
+  def predict(self, data, prepared = False, **kwargs):
+    if prepared:
+      gt = self.simfunc.get_and_resolve_prepared(data)
+    else:
+      gt = torch.zeros((len(data), self.simfunc.outdim), device = self.device)
+      for i in range(len(data)):
+        sim_promise = copy.deepcopy(self.simfunc)
+        sim_promise.get(data[i])
+        sim_spec = sim_promise.resolve()
+        gt[i, :] = np.mean(sim_spec[np.array(sim_promise.mask)], axis = 0)
+
     unc = torch.zeros(gt.size(), device = gt.device)
 
     return torch.stack((gt, unc))
